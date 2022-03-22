ddbc31661ac7dda0e24e2993e484bbd8ebcabda6
Rafael <rafaelsrios12@gmail.com>
2022-03-22 10:49:27 -0300

    Criação de sobrescrita de método de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,7 +6,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -18,7 +18,9 @@ class Pessoa:
 
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
 
 class Mutante(Pessoa):
     olhos = 3
@@ -48,3 +50,5 @@ if __name__ == '__main__':
     print(isinstance(pessoa, Pessoa))
     print(isinstance(pessoa, Homem))
     print(rafael.olhos)
+    print(luciano.cumprimentar())
+    print(rafael.cumprimentar())
